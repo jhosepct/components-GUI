abdb8ae308441ca5eea251c36dee641d2dcaec11
k3v1n <kevinjhosepct@gmail.com>
2023-01-05 01:04:20 -0500

    Modified design and size to components

--- src/DetailsMovieDialog.java
+++ src/DetailsMovieDialog.java
@@ -1,43 +1,116 @@
-public class DetailsMovieDialog extends javax.swing.JDialog {
-private javax.swing.JPanel contentPane;
-private javax.swing.JButton buttonOK;
-private javax.swing.JButton buttonCancel;
+import Movie.Pelicula;
 
-public DetailsMovieDialog(){
-setContentPane(contentPane);
-setModal(true);
-getRootPane().setDefaultButton(buttonOK);
+import javax.swing.*;
+import java.awt.event.*;
+import java.net.URL;
 
-buttonOK.addActionListener(new java.awt.event.ActionListener(){public void actionPerformed(java.awt.event.ActionEvent e){onOK();}});
+public class DetailsMovieDialog extends JDialog {
+    private JPanel mainPanel;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JButton imageButton;
+    private JTextArea sinopsisDescription;
+    private JLabel sinopsisLabel;
+    private JLabel directorLabel;
+    private JLabel directorDescription;
+    private JLabel generoDescription;
+    private JTextArea titleDescription;
+    private JPanel containerPanel;
+    private JPanel actionsPanel;
+    private JLabel generoLabel;
+    private JLabel plusDetails;
+    private javax.swing.JScrollPane JScrollPane;
 
-buttonCancel.addActionListener(new java.awt.event.ActionListener(){public void actionPerformed(java.awt.event.ActionEvent e){onCancel();}});
+    public DetailsMovieDialog(Pelicula pelicula) {
+        setContentPane(mainPanel);
+        setLayoutManager();
+        setLocationAndSize();
 
- // call onCancel() when cross is clicked
-setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-addWindowListener(new java.awt.event.WindowAdapter() {
-  public void windowClosing(java.awt.event.WindowEvent e) {
-   onCancel();
-  }
-});
+        try{
+            imageButton.setIcon(new ImageIcon(new URL(pelicula.getImagen())));
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        sinopsisDescription.setText(pelicula.getSinopsis());
+        generoDescription.setText(pelicula.getGenero());
 
- // call onCancel() on ESCAPE
-contentPane.registerKeyboardAction(  new java.awt.event.ActionListener() {    public void actionPerformed(java.awt.event.ActionEvent e) {      onCancel();
-    }  },  javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0),  javax.swing.JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);}
+        plusDetails.setText(pelicula.getDuracion() + " | "+ pelicula.getClasificacion());
+        directorDescription.setText(pelicula.getDirector());
+        titleDescription.setText(pelicula.getNombre());
 
-private void onOK(){
- // add your code here
-dispose();
-}
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
 
-private void onCancel(){
- // add your code here if necessary
-dispose();
-}
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
 
-public static void main(String[] args){
-DetailsMovieDialog dialog = new DetailsMovieDialog();
-dialog.pack();
-dialog.setVisible(true);
-System.exit(0);
-}
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        mainPanel.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        setBounds(10,10,650,650);
+        setResizable(false);
+
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public void setLayoutManager() {
+        containerPanel.setLayout(null);
+    }
+
+    public void setLocationAndSize() {
+        //Setting location and Size of each components using setBounds() method.
+        titleDescription.setBounds(50, 10, 500, 150);
+        imageButton.setBounds(50, 150, 300, 400);
+        sinopsisLabel.setBounds(400, 150, 200, 30);
+        JScrollPane.setBounds(400,200,150,130);
+        sinopsisDescription.setBounds(400, 200, 150, 130);
+        directorLabel.setBounds(400, 350, 200, 30);
+        directorDescription.setBounds(400, 380, 200, 30);
+        generoLabel.setBounds(400, 410, 200, 30);
+        generoDescription.setBounds(400, 440, 200, 30);
+        plusDetails.setBounds(400,520,200,30);
+
+        //Quita los bordes del contenedor scroll
+        JScrollPane.setBorder(BorderFactory.createEmptyBorder());
+
+    }
+
+
+    public static void main(String[] args) {
+        Pelicula pelicula1 = new Pelicula("El payaso siniestro terrifier 2", "Terror", "138min", "M14", "Damien Leone", "Después de que una entidad siniestra lo resucitase, Art the Clown está de vuelta en el condado de Miles, donde busca cazar a una adolescente y a su hermano menor durante Halloween.", "https://cinemarkmedia.modyocdn.com/pe/300x400/91336.jpg?version=1672790400000");
+        DetailsMovieDialog dialog = new DetailsMovieDialog(pelicula1);
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+
 }
